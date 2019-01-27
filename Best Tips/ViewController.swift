74e85c95dc0890072960f257dd75708f50f2ded8
Alan Petiote <AlanPetiote@alans-macbook-pro-4.local>
2019-01-27 10:08:54 -0500

    Initial Commit

---
+++ Best Tips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Best Tips
+//
+//  Created by Alan Petiote on 1/27/19.
+//  Copyright © 2019 APVentures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
